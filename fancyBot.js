5eaaf9a42a86498bd538d7d764c05cc37736d362
LolloMira <lorenzo23082000@gmail.com>
2022-02-18 13:09:49 +0100

    push

--- fancyBot.js
+++ fancyBot.js
@@ -8,30 +8,6 @@ class FancyBot extends BaseBot {
     }
 
     play() {
-
-        if (this.gameHistory.length < 2) {
-            return 0;
-        }
-        let enemySilence = 0;
-        let enemyTreason = 0;
-
-        for (let i = 0; i < this.gameHistory.length; i++) {
-            if (this.gameHistory[i] === 1) {
-                enemySilence++
-            } else {
-                enemyTreason++
-            }
-        }
-
-        if (enemyTreason >= 3 && enemyTreason - enemySilence >= 3) {
-            return 0
-        } 
-        
-        if (enemySilence >= 3 && enemySilence - enemyTreason >= 3) {
-            return (Math.random() < 0.5 ? 0 : 1)
-        }
-
-        return (Math.random() < 0.8 ? 1 : 0)
     }
 
     
